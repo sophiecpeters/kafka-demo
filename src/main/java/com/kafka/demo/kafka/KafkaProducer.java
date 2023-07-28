ebf60ae659d71b799600526faf78d393d7efd260
SP-Soof <sophie_c_peters@hotmail.com>
2023-07-28 18:03:10 +0200

    initial commit

---
+++ src/main/java/com/kafka/demo/kafka/KafkaProducer.java
@@ -0,0 +1,4 @@
+package com.kafka.demo.kafka;
+
+public class KafkaProducer {
+}
---
+++ src/main/java/com/kafka/demo/resource/Resource.java
@@ -0,0 +1,11 @@
+package com.kafka.demo.resource;
+
+import lombok.Data;
+
+@Data
+public class Resource {
+
+    public String id;
+    public String kind;
+
+}
